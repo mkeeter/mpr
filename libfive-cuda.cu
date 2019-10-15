b1968a450b6d0e2c7fabd0c58b7b99b7c378ebca
Matt Keeter <matt.j.keeter@gmail.com>
2019-10-15 08:48:53 -0400

    Add helper function to flatten and registerify tape

--- libfive-cuda.cu
+++ libfive-cuda.cu
@@ -1,7 +1,6 @@
 // System includes
 #include <stdio.h>
 #include <assert.h>
-#include <libfive/tree/opcode.hpp>
 
 // CUDA runtime
 #include <cuda_runtime.h>
@@ -10,6 +9,10 @@
 // Helper functions and utilities to work with CUDA
 #include <helper_functions.h>
 #include <helper_cuda.h>
+
+// libfive
+#include <libfive/tree/opcode.hpp>
+#include <libfive/tree/tree.hpp>
 
 // Our Interval arithmetic class
 #include "gpu_interval.hpp"
@@ -22,9 +25,15 @@ struct Clause {
     const uint16_t rhs;
 };
 
+// The Tape is an on-device representation, so the pointers
+// are returned from cudaMalloc.
 struct Tape {
     const Clause* const __restrict__ tape;
     const uint32_t tape_length;
+
+    const uint16_t num_regs;
+    const uint16_t num_csg_choices;
+
     const float* const __restrict__ constants;
 };
 
@@ -104,11 +113,9 @@ const static uint32_t THREADS_PER_BLOCK = TILE_COUNT / NUM_BLOCKS;
 __global__ void processTiles(const Tape tape,
         // Flat array for all pseudoregisters
         Interval* const __restrict__ regs_,
-        const uint32_t num_regs,
 
         // Flat array for all CSG choices
         uint8_t* const __restrict__ csg_choices_,
-        const uint32_t num_csg_choices,
 
         // Output data
         Output* const __restrict__ out)
@@ -121,8 +128,8 @@ __global__ void processTiles(const Tape tape,
 
     // Unpack a 1D offset into the data arrays
     const uint32_t index = x * TILE_COUNT + y;
-    Interval* __restrict__ const regs = regs_ + index * num_regs;
-    uint8_t* __restrict__ const csg_choices = csg_choices_ + index * num_csg_choices;
+    auto regs = regs_ + index * tape.num_regs;
+    auto csg_choices = csg_choices_ + index * tape.num_csg_choices;
     walk(tape, X, Y, regs, csg_choices);
 
     const Interval result = regs[tape.tape[tape.tape_length - 1].out];
@@ -130,6 +137,124 @@ __global__ void processTiles(const Tape tape,
             X.lower, X.upper,
             Y.lower, Y.upper,
             result.lower, result.upper);
+}
+
+Tape prepareTape(libfive::Tree tree) {
+    auto ordered = tree.ordered();
+
+    std::map<libfive::Tree::Id, libfive::Tree::Id> last_used;
+    std::vector<float> constant_data;
+    std::map<libfive::Tree::Id, uint16_t> constants;
+    uint16_t num_csg_choices = 0;
+    for (auto& c : ordered) {
+        if (c->op == libfive::Opcode::CONSTANT) {
+            // Store constants in a separate list
+            constants.insert({c.id(), constant_data.size()});
+            constant_data.push_back(c->value);
+        } else {
+            // Very simple tracking of active spans, without clause reordering
+            // or any other cleverness.
+            last_used.insert({c.lhs().id(), c.id()});
+            last_used.insert({c.rhs().id(), c.id()});
+
+            num_csg_choices += (c->op == libfive::Opcode::OP_MIN ||
+                                c->op == libfive::Opcode::OP_MAX);
+        }
+    }
+
+    std::list<uint16_t> free_registers;
+    std::map<libfive::Tree::Id, uint16_t> bound_registers;
+    uint16_t num_registers = 0;
+    std::vector<Clause> flat;
+    for (auto& c : ordered) {
+        // Constants are not inserted into the tape, because they
+        // live in a separate data array addressed with flags in
+        // the 'banks' argument of a Clause.
+        if (constants.find(c.id()) != constants.end()) {
+            continue;
+        }
+
+        // Pick a registers for the output of this opcode
+        uint16_t out;
+        if (free_registers.size()) {
+            out = free_registers.back();
+            free_registers.pop_back();
+        } else {
+            out = num_registers++;
+            if (num_registers == UINT16_MAX) {
+                fprintf(stderr, "Ran out of registers!\n");
+            }
+        }
+        bound_registers.insert({c.id(), out});
+
+        uint8_t banks = 0;
+        auto f = [&](libfive::Tree::Id id, uint8_t mask) {
+            if (id == nullptr) {
+                return static_cast<uint16_t>(0);
+            }
+            {   // Check whether this is a constant
+                auto itr = constants.find(id);
+                if (itr != constants.end()) {
+                    banks |= mask;
+                    return itr->second;
+                }
+            }
+            {   // Otherwise, it must be a bound register
+                auto itr = bound_registers.find(id);
+                if (itr != bound_registers.end()) {
+                    return itr->second;
+                } else {
+                    fprintf(stderr, "Could not LHS?");
+                    return static_cast<uint16_t>(0);
+                }
+            }
+        };
+
+        const uint16_t lhs = f(c.lhs().id(), 1);
+        const uint16_t rhs = f(c.rhs().id(), 2);
+
+        flat.push_back({static_cast<uint8_t>(c->op), banks, out, lhs, rhs});
+
+        std::cout << libfive::Opcode::toString(c->op) << " "
+                  << ((banks & 1) ? constant_data[lhs] : lhs) << " "
+                  << ((banks & 2) ? constant_data[rhs] : rhs) << " -> " << out << "\n";
+
+        // Release registers if this was their last use
+        for (auto& h : {c.lhs().id(), c.rhs().id()}) {
+            if (h != nullptr && h->op != libfive::Opcode::CONSTANT &&
+                last_used[h] == c.id())
+            {
+                auto itr = bound_registers.find(h);
+                free_registers.push_back(itr->second);
+                bound_registers.erase(itr);
+            }
+        }
+    }
+
+    Clause* d_flat_tape;
+    checkCudaErrors(cudaMalloc(
+                reinterpret_cast<void **>(&d_flat_tape),
+                sizeof(Clause) * flat.size()));
+    checkCudaErrors(cudaMemcpy(d_flat_tape, flat.data(),
+                sizeof(Clause) * flat.size(),
+                cudaMemcpyHostToDevice));
+
+    float* d_flat_constants;
+    checkCudaErrors(cudaMalloc(
+                reinterpret_cast<void **>(&d_flat_constants),
+                sizeof(float) * constant_data.size()));
+    checkCudaErrors(cudaMemcpy(
+                d_flat_constants, constant_data.data(),
+                sizeof(float) * constant_data.size(),
+                cudaMemcpyHostToDevice));
+
+    return Tape {
+        d_flat_tape,
+        static_cast<uint32_t>(flat.size()),
+        num_registers,
+        num_csg_choices,
+        d_flat_constants
+    };
 }
 
 /**
@@ -396,10 +521,18 @@ int main(int argc, char **argv) {
         printf("Min grid size: %i\tBlock size: %i\n", min_grid_size, block_size);
     }
 
+    {
+        auto X = libfive::Tree::X();
+        auto Y = libfive::Tree::Y();
+        auto circle = sqrt(X*X + Y*Y) - 1.0;
+        prepareTape(circle);
+    }
+    return 0;
+
     processTiles <<< grid, threads >>>(Tape {},
-        nullptr /* regs */, 0 /* num_regs */,
-        nullptr /* csg_choices */, 0 /* num_csg_choices */,
-        nullptr /* out */);
+        nullptr, /* regs */
+        nullptr, /* csg_choices */
+        nullptr  /* out */);
 
     printf("[Matrix Multiply Using CUDA] - Starting...\n");
 
