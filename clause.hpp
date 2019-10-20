cc7e3e0b11976700b7b0ec602ae6725f7ad75857
Matt Keeter <matt.j.keeter@gmail.com>
2019-10-19 20:53:21 -0400

    Don't enforce constant-ness on Clauses

--- clause.hpp
+++ clause.hpp
@@ -2,9 +2,9 @@
 #include <cstdint>
 
 struct Clause {
-    const uint8_t opcode;
-    const uint8_t banks;
-    const uint16_t out;
-    const uint16_t lhs;
-    const uint16_t rhs;
+    uint8_t opcode;
+    uint8_t banks;
+    uint16_t out;
+    uint16_t lhs;
+    uint16_t rhs;
 };
