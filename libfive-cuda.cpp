a7e2ef99e2a7ce699a6878420e13c85ce62b5977
Matt Keeter <matt.j.keeter@gmail.com>
2019-10-21 19:34:50 -0400

    Add option to run with custom frep file

--- libfive-cuda.cpp
+++ libfive-cuda.cpp
@@ -1,24 +1,37 @@
-// System includes
 #include <stdio.h>
 #include <assert.h>
+#include <iostream>
+#include <fstream>
 
 // CUDA runtime
 #include <cuda_runtime.h>
-#include <math_constants.h>
 
 // libfive
-#include <libfive/tree/opcode.hpp>
 #include <libfive/tree/tree.hpp>
+#include <libfive/tree/archive.hpp>
 
 #include "renderable.hpp"
 
 int main(int argc, char **argv)
 {
-    auto X = libfive::Tree::X();
-    auto Y = libfive::Tree::Y();
-    auto circle = min(sqrt((X + 1.5)*(X + 1.5)+ Y*Y) - 1.0,
-        sqrt((X - 1.5)*(X - 1.5) + Y*Y) - 1.0);
-    auto r = Renderable::build(circle, 4096);
+    libfive::Tree t = libfive::Tree::X();
+    if (argc == 2) {
+        std::ifstream ifs;
+        ifs.open(argv[1]);
+        if (ifs.is_open()) {
+            auto a = libfive::Archive::deserialize(ifs);
+            t = a.shapes.front().tree;
+        } else {
+            fprintf(stderr, "Could not open file %s\n", argv[1]);
+            exit(1);
+        }
+    } else {
+        auto X = libfive::Tree::X();
+        auto Y = libfive::Tree::Y();
+        t= min(sqrt((X + 1.5)*(X + 1.5)+ Y*Y) - 1.0,
+               sqrt((X - 1.5)*(X - 1.5) + Y*Y) - 1.0);
+    }
+    auto r = Renderable::build(t, 256);
     for (unsigned i=0; i < 10; ++i) {
         r->run({{0, 0}, 2});
         cudaDeviceSynchronize();
