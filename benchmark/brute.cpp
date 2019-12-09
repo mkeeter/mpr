543ee3c050d5af727fe1ddfd6c828c0704fa2546
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-08 21:29:15 -0500

    Changes to brute-force benchmark on Martin's server

--- benchmark/brute.cpp
+++ benchmark/brute.cpp
@@ -30,17 +30,39 @@ int main(int argc, char **argv)
         t = min(sqrt((X + 0.5)*(X + 0.5) + Y*Y + Z*Z) - 0.25,
                 sqrt((X - 0.5)*(X - 0.5) + Y*Y + Z*Z) - 0.25);
     }
-    auto r_ = Renderable::build(t, 1024, 2);
-    auto r = dynamic_cast<Renderable2D*>(r_.get());
 
+    std::cout << "Rendering brute-force with interpreter\n";
+    for (unsigned i=256; i <= 2048; i += 64)
     {
-        auto start_gpu = std::chrono::steady_clock::now();
-        r->runBrute({Eigen::Matrix4f::Identity()});
-        auto end_gpu = std::chrono::steady_clock::now();
+        auto r_ = Renderable::build(t, i, 2);
+        auto r = dynamic_cast<Renderable2D*>(r_.get());
+        // Warm up
+        for (unsigned i=0; i < 10; ++i) {
+            r->runBrute({Eigen::Matrix4f::Identity()});
+        }
+        // Benchmark
+        std::vector<double> times_ms;
+        for (unsigned i=0; i < 50; ++i) {
+            auto start_gpu = std::chrono::steady_clock::now();
+            r->runBrute({Eigen::Matrix4f::Identity()});
+            auto end_gpu = std::chrono::steady_clock::now();
+            times_ms.push_back(
+                    std::chrono::duration_cast<std::chrono::nanoseconds>(end_gpu - start_gpu).count()
+                    / 1e6
+                    );
+        }
+        double mean = 0;
+        for (auto& b : times_ms) {
+            mean += b;
+        }
+        mean /= times_ms.size();
+        double stdev = 0;
+        for (auto& b : times_ms) {
+            stdev += std::pow(b - mean, 2);
+        }
+        stdev = sqrt(stdev / (times_ms.size() - 1));
+        std::cout << i << " " << mean << " " << stdev << "\n";
 
-        std::cout << "GPU rendering took " <<
-            std::chrono::duration_cast<std::chrono::milliseconds>(end_gpu - start_gpu).count() <<
-            " ms\n";
         // Save the image using libfive::Heightmap
         libfive::Heightmap out(r->image.size_px, r->image.size_px);
         for (unsigned x=0; x < r->image.size_px; ++x) {
@@ -48,16 +70,42 @@ int main(int argc, char **argv)
                 out.depth(y, x) = r->heightAt(x, y);
             }
         }
-        out.savePNG("out_brute.png");
+        out.savePNG("out_brute_" + std::to_string(i) + ".png");
     }
-    {
-        auto start_gpu = std::chrono::steady_clock::now();
-        r->runBruteKernel({Eigen::Matrix4f::Identity()});
-        auto end_gpu = std::chrono::steady_clock::now();
+    return 0;
 
-        std::cout << "GPU rendering took " <<
-            std::chrono::duration_cast<std::chrono::milliseconds>(end_gpu - start_gpu).count() <<
-            " ms\n";
+    std::cout << "Rendering hard-compiled kernel\n";
+    for (unsigned i=256; i <= 4096; i += 64)
+    {
+        auto r_ = Renderable::build(t, i, 2);
+        auto r = dynamic_cast<Renderable2D*>(r_.get());
+        // Warm up
+        for (unsigned i=0; i < 10; ++i) {
+            r->runBruteKernel({Eigen::Matrix4f::Identity()});
+        }
+        // Benchmark
+        std::vector<double> times_ms;
+        for (unsigned i=0; i < 100; ++i) {
+            auto start_gpu = std::chrono::steady_clock::now();
+            r->runBruteKernel({Eigen::Matrix4f::Identity()});
+            auto end_gpu = std::chrono::steady_clock::now();
+            times_ms.push_back(
+                    std::chrono::duration_cast<std::chrono::nanoseconds>(end_gpu - start_gpu).count()
+                    / 1e6
+                    );
+        }
+        double mean = 0;
+        for (auto& b : times_ms) {
+            mean += b;
+        }
+        mean /= times_ms.size();
+        double stdev = 0;
+        for (auto& b : times_ms) {
+            stdev += std::pow(b - mean, 2);
+        }
+        stdev = sqrt(stdev / (times_ms.size() - 1));
+        std::cout << i << " " << mean << " " << stdev << "\n";
+
         // Save the image using libfive::Heightmap
         libfive::Heightmap out(r->image.size_px, r->image.size_px);
         for (unsigned x=0; x < r->image.size_px; ++x) {
@@ -65,17 +113,40 @@ int main(int argc, char **argv)
                 out.depth(y, x) = r->heightAt(x, y);
             }
         }
-        out.savePNG("out_brute_kernel.png");
+        out.savePNG("out_kernel_" + std::to_string(i) + ".png");
     }
 
+    std::cout << "Rendering fancy algorithm with interpreter\n";
+    for (unsigned i=256; i <= 4096; i += 64)
     {
-        auto start_gpu = std::chrono::steady_clock::now();
-        r->run({Eigen::Matrix4f::Identity()});
-        auto end_gpu = std::chrono::steady_clock::now();
+        auto r_ = Renderable::build(t, i, 2);
+        auto r = dynamic_cast<Renderable2D*>(r_.get());
+        for (unsigned i=0; i < 10; ++i) {
+            r->run({Eigen::Matrix4f::Identity()});
+        }
+        // Benchmark
+        std::vector<double> times_ms;
+        for (unsigned i=0; i < 100; ++i) {
+            auto start_gpu = std::chrono::steady_clock::now();
+            r->run({Eigen::Matrix4f::Identity()});
+            auto end_gpu = std::chrono::steady_clock::now();
+            times_ms.push_back(
+                    std::chrono::duration_cast<std::chrono::nanoseconds>(end_gpu - start_gpu).count()
+                    / 1e6
+                    );
+        }
+        double mean = 0;
+        for (auto& b : times_ms) {
+            mean += b;
+        }
+        mean /= times_ms.size();
+        double stdev = 0;
+        for (auto& b : times_ms) {
+            stdev += std::pow(b - mean, 2);
+        }
+        stdev = sqrt(stdev / (times_ms.size() - 1));
+        std::cout << i << " " << mean << " " << stdev << "\n";
 
-        std::cout << "GPU rendering took " <<
-            std::chrono::duration_cast<std::chrono::milliseconds>(end_gpu - start_gpu).count() <<
-            " ms\n";
         // Save the image using libfive::Heightmap
         libfive::Heightmap out(r->image.size_px, r->image.size_px);
         for (unsigned x=0; x < r->image.size_px; ++x) {
@@ -83,6 +154,6 @@ int main(int argc, char **argv)
                 out.depth(y, x) = r->heightAt(x, y);
             }
         }
-        out.savePNG("out_full.png");
+        out.savePNG("out_alg_" + std::to_string(i) + ".png");
     }
 }
