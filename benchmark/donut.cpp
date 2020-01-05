e21cbec05078e55f70f64f5e20826d82c062d99b
Matt Keeter <matt.j.keeter@gmail.com>
2020-01-05 10:48:08 -0500

    Modify donut to also print constants, and accept a tree from argv

--- benchmark/donut.cpp
+++ benchmark/donut.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <fstream>
+
 // libfive
 #include <libfive/tree/tree.hpp>
 #include <libfive/tree/archive.hpp>
@@ -6,11 +9,23 @@
 #include "renderable.hpp"
 
 // Not actually a benchmark, used to generate data for the paper
-int main(int, char**)
+int main(int argc, char** argv)
 {
     auto X = libfive::Tree::X();
     auto Y = libfive::Tree::Y();
     auto t = max(sqrt(X*X + Y*Y) - 1, 0.5 - sqrt(X*X + Y*Y));
+
+    if (argc == 2) {
+        std::ifstream ifs;
+        ifs.open(argv[1]);
+        if (ifs.is_open()) {
+            auto a = libfive::Archive::deserialize(ifs);
+            t = a.shapes.front().tree;
+        } else {
+            fprintf(stderr, "Could not open file %s\n", argv[1]);
+            exit(1);
+        }
+    }
     auto r = Renderable::build(t, 128, 2);
 
     for (unsigned i=0; i < r->tape.num_clauses; ++i) {
@@ -18,6 +33,11 @@ int main(int, char**)
         std::cout << libfive::Opcode::toString((libfive::Opcode::Opcode)c.opcode) << " & "
                   << ((c.banks & 1) ? "constant " : "slot ") << c.lhs << " & "
                   << ((c.banks & 2) ? "constant " : "slot ") << c.rhs << " & "
-                  << c.out << "\n";
+                  << c.out << "\\\\\n";
+    }
+    std::cout << "\n\n";
+    for (unsigned i=0; i < r->tape.num_constants; ++i) {
+        const float f = r->tape.constant(i);
+        std::cout << i << " & " << f << "\\\\\n";
     }
 }
