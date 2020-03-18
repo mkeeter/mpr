c9a6cb1ab94095c619ffb24a99c3e8e50104d77c
Matt Keeter <matt.j.keeter@gmail.com>
2020-03-17 20:38:49 -0400

    Make macros slightly safer

--- inc/clause.hpp
+++ inc/clause.hpp
@@ -4,9 +4,9 @@
 //
 // These macros let us address individual parts of that value, in a way
 // which almost definitely is Undefined Behavior.
-#define OP(d) (((uint8_t*)d)[0])
-#define I_OUT(d) (((uint8_t*)d)[1])
-#define I_LHS(d) (((uint8_t*)d)[2])
-#define I_RHS(d) (((uint8_t*)d)[3])
-#define IMM(d) (((float*)d)[1])
-#define JUMP_TARGET(d) (((int32_t*)d)[1])
+#define OP(d) (((uint8_t*)(d))[0])
+#define I_OUT(d) (((uint8_t*)(d))[1])
+#define I_LHS(d) (((uint8_t*)(d))[2])
+#define I_RHS(d) (((uint8_t*)(d))[3])
+#define IMM(d) (((float*)(d))[1])
+#define JUMP_TARGET(d) (((int32_t*)(d))[1])
