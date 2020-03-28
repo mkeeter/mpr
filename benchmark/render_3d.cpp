0fcf342f41767437c38bf65798b72d446e1e708e
Matt Keeter <matt.j.keeter@gmail.com>
2020-03-28 18:39:43 -0400

    Fix typo in argument parsing

--- benchmark/render_3d.cpp
+++ benchmark/render_3d.cpp
@@ -14,7 +14,7 @@
 int main(int argc, char **argv)
 {
     libfive::Tree t = libfive::Tree::X();
-    if (argc == 2) {
+    if (argc >= 2) {
         std::ifstream ifs;
         ifs.open(argv[1]);
         if (ifs.is_open()) {
