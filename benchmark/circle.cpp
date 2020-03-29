d2dc79d3491955c00e1b3519c5dd61d661dc843a
Matt Keeter <matt.j.keeter@gmail.com>
2020-03-29 13:56:10 -0400

    Update circle oneshot to use new implementation

--- benchmark/circle.cpp
+++ benchmark/circle.cpp
@@ -3,8 +3,8 @@
 #include <libfive/tree/archive.hpp>
 #include <libfive/render/discrete/heightmap.hpp>
 
-#define protected public
-#include "renderable.hpp"
+#include "context.hpp"
+#include "tape.hpp"
 
 // Not actually a benchmark, used to generate a figure for the paper
 int main(int, char**)
@@ -13,68 +13,79 @@ int main(int, char**)
     auto Y = libfive::Tree::Y();
     auto t = sqrt((X + 1)*(X + 1) + (Y + 1)*(Y + 1)) - 1.8;
 
-    auto r_ = Renderable::build(t, 128, 2);
-    auto r = dynamic_cast<Renderable2D*>(r_.get());
-    r->run({Eigen::Matrix4f::Identity()}, Renderable::MODE_HEIGHTMAP);
+    const auto size = 128;
+    auto ctx = libfive::cuda::Context(128);
+    auto tape = libfive::cuda::Tape(t);
+    ctx.render2D(tape, Eigen::Matrix3f::Identity());
 
-    // Save the image using libfive::Heightmap
-    libfive::Heightmap out(r->image.size_px, r->image.size_px);
-    for (unsigned x=0; x < r->image.size_px; ++x) {
-        for (unsigned y=0; y < r->image.size_px; ++y) {
-            out.depth(y, x) = r->heightAt(x, y);
+    libfive::Heightmap out(size, size);
+
+    constexpr static auto COLOR_GREY = 0xFF636363;
+    constexpr static auto COLOR_YELLOW = 0xFF21F9F0;
+    constexpr static auto COLOR_ORANGE = 0xFF3E9AF8;
+    constexpr static auto COLOR_BLUE = 0xFF880710;
+    constexpr static auto COLOR_PINK = 0xFF6264E1;
+    constexpr static auto COLOR_PURPLE = 0xFFA8006E;
+
+    out.norm = COLOR_GREY; // Grey
+
+    for (unsigned i=0; i < pow(size / 64, 2); ++i) {
+        const auto tile = ctx.stages[0].tiles[i];
+        if (tile.position != -1) {
+            continue;
         }
-    }
+        const uint32_t x = i % (size / 64);
+        const uint32_t y = i / (size / 64);
 
-    unsigned index=0;
-    out.norm = 0xFF000055;
-    for (unsigned x=0; x < 2; ++x) {
-        for (unsigned y=0; y < 2; ++y) {
-            if (r->filled_tiles.filled(index++)) {
-                for (unsigned i=x*64; i < (x+1)*64; ++i) {
-                    for (unsigned j=y*64; j < (y+1)*64; ++j) {
-                        out.norm(j,i) = 0xFFFFFFFF;
-                    }
-                }
+        for (unsigned i=x*64; i < (x+1)*64; ++i) {
+            for (unsigned j=y*64; j < (y+1)*64; ++j) {
+                out.norm(j,i) = COLOR_YELLOW;
             }
         }
     }
-    out.saveNormalPNG("circle2.png");
-    index = 0;
-    for (unsigned x=0; x < 16; ++x) {
-        for (unsigned y=0; y < 16; ++y) {
-            if (r->filled_subtiles.filled(index++)) {
-                for (unsigned i=x*8; i < (x+1)*8; ++i) {
-                    for (unsigned j=y*8; j < (y+1)*8; ++j) {
-                        out.norm(j,i) = 0xFFAAAAAA;
-                    }
-                }
-            }
-        }
-    }
-    for (unsigned x=0; x < 16; ++x) {
-        for (unsigned y=0; y < 16; ++y) {
-            if (r->filled_subtiles.filled(index++)) {
-                for (unsigned i=x*8; i < (x+1)*8; ++i) {
-                    for (unsigned j=y*8; j < (y+1)*8; ++j) {
-                        out.norm(j,i) = 0xFFAAAAAA;
-                    }
-                }
-            }
-        }
-    }
-    out.saveNormalPNG("circle2.png");
+    out.saveNormalPNG("circle1.png");
 
-    for (unsigned i=0; i < r->queue_pong.count; ++i) {
-        const uint32_t subtile = r->queue_pong.data[i];
-        const uint32_t subtile_pos = r->pixel_renderer.subtiles.pos(subtile);
-        const uint32_t x = subtile_pos % (128/8);
-        const uint32_t y = subtile_pos / (128/8);
+    for (unsigned i=0; i < ctx.stages[2].tile_array_size; ++i) {
+        const auto tile = ctx.stages[2].tiles[i];
+        if (tile.position != -1) {
+            continue;
+        }
+
+        int q = -1;
+        unsigned parent = -1;
+        for (unsigned j=0; j < pow(size / 64, 2); ++j) {
+            if (ctx.stages[0].tiles[j].position != -1) {
+                q++;
+            }
+            if (q == i / 64) {
+                parent = ctx.stages[0].tiles[j].position;
+                break;
+            }
+        }
+        auto px = parent % (size / 64);
+        auto py = parent / (size / 64);
+        auto x = (px * 8) + ((i % 64) % 8);
+        auto y = (py * 8) + ((i % 64) / 8);
+
         for (unsigned i=x*8; i < (x+1)*8; ++i) {
             for (unsigned j=y*8; j < (y+1)*8; ++j) {
-                if (out.depth(j,i)) {
-                    out.norm(j,i) = 0xFFAA6666;
+                if (ctx.stages[3].filled[i + j * size]) {
+                    out.norm(j,i) = COLOR_ORANGE;
                 } else {
-                    out.norm(j,i) = 0xFF000000;
+                    out.norm(j,i) = COLOR_BLUE;
+                }
+            }
+        }
+    }
+    out.saveNormalPNG("circle2.png");
+
+    for (unsigned i=0; i < size; ++i) {
+        for (unsigned j=0; j < size; ++j) {
+            if (out.norm(j, i) == COLOR_GREY) {
+                if (ctx.stages[3].filled[i + j * size]) {
+                    out.norm(j, i) = COLOR_PINK;
+                } else {
+                    out.norm(j, i) = COLOR_PURPLE;
                 }
             }
         }
