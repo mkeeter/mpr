43423748c632effffc647b085ad8344ca9af3cf6
Matt Keeter <matt.j.keeter@gmail.com>
2021-06-29 21:38:32 -0400

    Bump libfive and get things compiling

--- src/tape.cpp
+++ src/tape.cpp
@@ -10,7 +10,7 @@ You can obtain one at http://mozilla.org/MPL/2.0/.
 Copyright (C) 2019-2020  Matt Keeter
 */
 #include "libfive/tree/tree.hpp"
-#include "libfive/tree/cache.hpp"
+#include "libfive/tree/data.hpp"
 
 #include "clause.hpp"
 #include "tape.hpp"
@@ -19,24 +19,22 @@ Copyright (C) 2019-2020  Matt Keeter
 namespace mpr {
 
 Tape::Tape(const libfive::Tree& tree) {
-    // Hold a single cache lock to avoid needing mutex locks everywhere
-    auto lock = libfive::Cache::instance();
-
-    auto ordered = tree.orderedDfs();
-    std::vector<libfive::Tree::Id> ordered_fast;
+    const auto tree_ = tree.optimized();
+    auto ordered = tree_.walk();
+    std::vector<const libfive::Tree::Data*> ordered_fast;
     ordered_fast.reserve(ordered.size());
 
     std::map<libfive::Tree::Id, libfive::Tree::Id> last_used;
-    libfive::Tree::Id axes_used[3] = {nullptr};
+    const libfive::Tree::Data* axes_used[3] = {nullptr};
     for (auto& c : ordered) {
         using namespace libfive::Opcode;
         // Very simple tracking of active spans, without clause reordering
         // or any other cleverness.
-        switch (c->op) {
+        switch (c->op()) {
             case CONSTANT: continue;
-            case VAR_X: axes_used[0] = c.id(); break;
-            case VAR_Y: axes_used[1] = c.id(); break;
-            case VAR_Z: axes_used[2] = c.id(); break;
+            case VAR_X: axes_used[0] = c; break;
+            case VAR_Y: axes_used[1] = c; break;
+            case VAR_Z: axes_used[2] = c; break;
 
             // Opcodes which take two arguments store their RHS
             case OP_ADD:
@@ -44,7 +42,7 @@ Tape::Tape(const libfive::Tree& tree) {
             case OP_MIN:
             case OP_MAX:
             case OP_SUB:
-            case OP_DIV:    last_used[c.rhs().id()] = c.id();
+            case OP_DIV:    last_used[c->rhs().id()] = c;
                             // FALLTHROUGH
 
             // Unary opcodes (and fallthrough) store their LHS)
@@ -58,18 +56,18 @@ Tape::Tape(const libfive::Tree& tree) {
             case OP_ATAN:
             case OP_EXP:
             case OP_ABS:
-            case OP_LOG:    last_used[c.lhs().id()] = c.id();
-                            ordered_fast.push_back(c.id());
+            case OP_LOG:    last_used[c->lhs().get()] = c;
+                            ordered_fast.push_back(c);
                             break;
             default:    break;
         }
     }
 
     std::vector<uint8_t> free_slots;
-    std::map<libfive::Tree::Id, uint8_t> bound_slots;
+    std::map<const libfive::Tree::Data*, uint8_t> bound_slots;
     uint8_t num_slots = 1;
 
-    auto getSlot = [&](libfive::Tree::Id id) {
+    auto getSlot = [&](const libfive::Tree::Data* id) {
         // Pick a slot for the output of this opcode
         uint8_t out = 0;
         if (free_slots.size()) {
@@ -98,19 +96,19 @@ Tape::Tape(const libfive::Tree& tree) {
     flat.reserve(ordered.size());
     flat.push_back(start);
 
-    auto get_reg = [&](const std::shared_ptr<libfive::Tree::Tree_>& tree) {
-        auto itr = bound_slots.find(tree.get());
+    auto get_reg = [&](const libfive::TreeData* tree) {
+        auto itr = bound_slots.find(tree);
         if (itr != bound_slots.end()) {
             return itr->second;
         } else {
-            fprintf(stderr, "Could not find bound slots %i\n", tree->op);
+            fprintf(stderr, "Could not find bound slots %i\n", tree->op());
             return static_cast<uint8_t>(0);
         }
     };
 
     for (auto& c : ordered_fast) {
         uint64_t clause = 0;
-        switch (c->op) {
+        switch (c->op()) {
             using namespace libfive::Opcode;
 
             case CONSTANT:
@@ -121,9 +119,9 @@ Tape::Tape(const libfive::Tree& tree) {
 
 #define OP_UNARY(p) \
             case OP_##p: { \
-                OP(&clause) = GPU_OP_##p##_LHS;      \
-                I_LHS(&clause) = get_reg(c->lhs);    \
-                break;                              \
+                OP(&clause) = GPU_OP_##p##_LHS;             \
+                I_LHS(&clause) = get_reg(c->lhs().get());   \
+                break;                                      \
             }
             OP_UNARY(SQUARE)
             OP_UNARY(SQRT);
@@ -139,18 +137,18 @@ Tape::Tape(const libfive::Tree& tree) {
 
 #define OP_COMMUTATIVE(p) \
             case OP_##p: { \
-                if (c->lhs->op == CONSTANT) {                   \
+                if (c->lhs()->op() == CONSTANT) {               \
                     OP(&clause) = GPU_OP_##p##_LHS_IMM;         \
-                    I_LHS(&clause) = get_reg(c->rhs);           \
-                    IMM(&clause) = c->lhs->value;               \
-                } else if (c->rhs->op == CONSTANT) {            \
+                    I_LHS(&clause) = get_reg(c->rhs().get());   \
+                    IMM(&clause) = c->lhs()->value();           \
+                } else if (c->rhs()->op() == CONSTANT) {        \
                     OP(&clause) = GPU_OP_##p##_LHS_IMM;         \
-                    I_LHS(&clause) = get_reg(c->lhs);           \
-                    IMM(&clause) = c->rhs->value;               \
+                    I_LHS(&clause) = get_reg(c->lhs().get());   \
+                    IMM(&clause) = c->rhs()->value();           \
                 } else {                                        \
                     OP(&clause) = GPU_OP_##p##_LHS_RHS;         \
-                    I_LHS(&clause) = get_reg(c->lhs);           \
-                    I_RHS(&clause) = get_reg(c->rhs);           \
+                    I_LHS(&clause) = get_reg(c->lhs().get());   \
+                    I_RHS(&clause) = get_reg(c->rhs().get());   \
                 }                                               \
                 break;                                          \
             }
@@ -161,18 +159,18 @@ Tape::Tape(const libfive::Tree& tree) {
 
 #define OP_NONCOMMUTATIVE(p) \
             case OP_##p: { \
-                if (c->lhs->op == CONSTANT) {                   \
+                if (c->lhs()->op() == CONSTANT) {               \
                     OP(&clause) = GPU_OP_##p##_IMM_RHS;         \
-                    I_RHS(&clause) = get_reg(c->rhs);           \
-                    IMM(&clause) = c->lhs->value;               \
-                } else if (c->rhs->op == CONSTANT) {            \
+                    I_RHS(&clause) = get_reg(c->rhs().get());   \
+                    IMM(&clause) = c->lhs()->value();           \
+                } else if (c->rhs()->op() == CONSTANT) {        \
                     OP(&clause) = GPU_OP_##p##_LHS_IMM;         \
-                    I_LHS(&clause) = get_reg(c->lhs);           \
-                    IMM(&clause) = c->rhs->value;               \
+                    I_LHS(&clause) = get_reg(c->lhs().get());   \
+                    IMM(&clause) = c->rhs()->value();           \
                 } else {                                        \
                     OP(&clause) = GPU_OP_##p##_LHS_RHS;         \
-                    I_LHS(&clause) = get_reg(c->lhs);           \
-                    I_RHS(&clause) = get_reg(c->rhs);           \
+                    I_LHS(&clause) = get_reg(c->lhs().get());   \
+                    I_RHS(&clause) = get_reg(c->rhs().get());   \
                 }                                               \
                 break;                                          \
             }
@@ -198,9 +196,19 @@ Tape::Tape(const libfive::Tree& tree) {
 
         // Release slots if this was their last use.  We do this now so
         // that one of them can be reused for the output slots below.
-        for (auto& h : {c->lhs.get(), c->rhs.get()}) {
-            if (h != nullptr &&
-                h->op != libfive::Opcode::CONSTANT &&
+        if (libfive::Opcode::args(c->op()) == 2) {
+            for (auto& h : {c->lhs().get(), c->rhs().get()}) {
+                if (h->op() != libfive::Opcode::CONSTANT &&
+                    last_used[h] == c)
+                {
+                    auto itr = bound_slots.find(h);
+                    free_slots.push_back(itr->second);
+                    bound_slots.erase(itr);
+                }
+            }
+        } else if (libfive::Opcode::args(c->op()) == 1) {
+            auto h = c->lhs().get();
+            if (h->op() != libfive::Opcode::CONSTANT &&
                 last_used[h] == c)
             {
                 auto itr = bound_slots.find(h);
@@ -216,7 +224,7 @@ Tape::Tape(const libfive::Tree& tree) {
     {   // Push the end of the tape, which points to the final clauses's
         // output slot so that we know where to read the result.
         uint64_t end = 0;
-        I_OUT(&end) = get_reg(ordered.back().operator->());
+        I_OUT(&end) = get_reg(ordered.back());
         flat.push_back(end);
     }
 
